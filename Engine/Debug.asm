2f3bd6d92a698501d369fc66ec928d746263f263
Robbe Mahieu <merendipity99@gmail.com>
2023-12-19 16:40:10 +0100

    Added debug function

---
+++ Engine/Debug.asm
@@ -0,0 +1,37 @@
+;-------------------------------------------------------------------------------------------------------------------
+; Debug functions - (c) Robbe Mahieu
+; 
+;-------------------------------------------------------------------------------------------------------------------
+
+; Includes
+ extern MessageBoxA
+ extern wsprintfA
+
+; Constants and Data
+section .data
+caption db "Register Value", 0
+formatString db "%d", 0                               ; Format string for a 32-bit hexadecimal value
+
+section .bss
+buffer resb 50
+
+;-------------------------------------------------------------------------------------------------------------------
+section .text                                           ; Code
+;-------------------------------------------------------------------------------------------------------------------
+
+DebugRegister:
+    enter 0,0
+    
+    push dword [ebp+8]
+    push formatString
+    push buffer
+    call wsprintfA 
+
+    push 0                                              ; Style
+    push caption                                        ; Caption
+    push buffer                                         ; Message
+    push 0                                              ; hWnd     
+    call MessageBoxA
+
+    leave
+    ret
