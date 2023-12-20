bbf56ec796bda22aff009f2fa74a74320d46923c
Robbe Mahieu <merendipity99@gmail.com>
2023-12-20 15:31:27 +0100

    Refactored input system To keep track of the previous state

--- Engine/Input.asm
+++ Engine/Input.asm
@@ -11,9 +11,14 @@
 %define HOLD    1
 %define RELEASE 3
 
+%define IN_KEYSTROKEMASK    0x00FF0000
+%define IN_KEYSTROKESHIFT   16
+%define IN_KEYSTATEMASK     0x80000000
+%define IN_KEYSTATESHIFT    31
+
 section .bss
-InputState resb 32
-InputHeld resb 32
+CurrentInputState resb 32
+PreviousInputState resb 32
 
 ;-------------------------------------------------------------------------------------------------------------------
 section .text                                           ; Code
@@ -22,8 +27,23 @@ section .text                                           ; Code
 InitInput:
     enter 0, 0
 
-    mov dword [InputState], 0
-    mov dword [InputHeld], 0
+    mov dword [PreviousInputState], 0
+    mov dword [PreviousInputState+4], 0
+    mov dword [PreviousInputState+8], 0
+    mov dword [PreviousInputState+12], 0
+    mov dword [PreviousInputState+16], 0
+    mov dword [PreviousInputState+20], 0
+    mov dword [PreviousInputState+24], 0
+    mov dword [PreviousInputState+28], 0
+
+    mov dword [CurrentInputState], 0
+    mov dword [CurrentInputState+4], 0
+    mov dword [CurrentInputState+8], 0
+    mov dword [CurrentInputState+12], 0
+    mov dword [CurrentInputState+16], 0
+    mov dword [CurrentInputState+20], 0
+    mov dword [CurrentInputState+24], 0
+    mov dword [CurrentInputState+28], 0
 
     leave
     ret
@@ -33,54 +53,69 @@ InitInput:
 UpdateInput:
     enter 0, 0
     push ebx
-    push esi
     push edi
 
     ; Get keystroke in eax
-    mov eax, 0x00FF0000                                 ; Mask
+    mov eax, IN_KEYSTROKEMASK
     and eax, [ebp+8]
-    shr eax, 16                                         ; Shift bits over
+    shr eax, IN_KEYSTROKESHIFT                          ; Shift bits over
 
     ; Get state
-    mov esi, 0xC0000000                                 ; Mask
-    and esi, [ebp+8]
-    shr esi, 30                                         ; Shift bits over
-    mov ecx, 0x00000001
-    and ecx, esi                                        ; Cache keystate in ecx
-    shr esi, 1                                          ; cache held in edx
+    mov ecx, IN_KEYSTATEMASK
+    and ecx, [ebp+8]
+    shr ecx, IN_KEYSTATESHIFT                           ; Shift bits over
+    xor ecx, 1                                          ; invert the pressed state (pressed = 1)
 
-    ; Save the state
+    ; Manipulate flags in correct position
     mov edi, 8                                          ; Get the correct byte
     div edi                                             ; eax / 8 => byte number is eax, offset in the byte is edx
-    mov ebx, 1                                          ; Create mask
-    xor ecx, 1
+    mov ebx, 1                                          ; Create bitmask
 
     .ShiftBits:                                         ; Put bits at correct position in byte
     cmp edx, 0
     jz .DoneShifting
 
-    shl ecx, 1
-    shl esi, 1                                          
+    shl ecx, 1                                       
     shl ebx, 1 
 
     sub edx, 1
     jmp .ShiftBits
 
-    .DoneShifting:
-    lea edx, [InputState]                               ; store address
+    .DoneShifting:                                      ; Save the state
+    lea edx, [CurrentInputState]                        ; store address
     add edx, eax                                        ; add offset
 
-    xor dword [edx], ebx                                ; Unset bit
+    xor ebx, 0xFFFFFFFF                                 ; Invert bitmask (keep everything but the bit)
+    and dword [edx], ebx                                ; Unset bit
     or dword [edx], ecx                                 ; Set it to the new state
 
-    lea edx, [InputHeld]                                ; store address
-    add edx, eax                                        ; add offset
-
-    xor dword [edx], ebx                                ; Unset bit
-    or dword [edx], esi                                 ; Set it to the new state
-
     pop edi
-    pop esi
     pop ebx
+    leave
+    ret
+
+HandleInput:
+    enter 0, 0
+
+    ; Check all bound actions
+
+    ; Update previous input to current input
+    lea edx, [CurrentInputState]
+    mov [PreviousInputState], edx
+    lea edx, [CurrentInputState+4]
+    mov [PreviousInputState+4], edx
+    lea edx, [CurrentInputState+8]
+    mov [PreviousInputState+8], edx
+    lea edx, [CurrentInputState+12]
+    mov [PreviousInputState+12], edx
+    lea edx, [CurrentInputState+16]
+    mov [PreviousInputState+16], edx
+    lea edx, [CurrentInputState+20]
+    mov [PreviousInputState+20], edx
+    lea edx, [CurrentInputState+24]
+    mov [PreviousInputState+24], edx
+    lea edx, [CurrentInputState+28]
+    mov [PreviousInputState+28], edx
+
     leave
     ret
