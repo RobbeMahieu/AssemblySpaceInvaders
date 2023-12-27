46e8f8467fd0caf56d63d11daad97083ae098a60
Robbe Mahieu <merendipity99@gmail.com>
2023-12-27 14:54:40 +0100

    Added resource copying to the batch script

--- Buildfile.bat
+++ Buildfile.bat
@@ -1,5 +1,6 @@
 SET BUILD_FOLDER=%~p0Build
 SET ENGINE_FOLDER=%~p0Engine
+SET RESOURCES_FOLDER=%~p0Resources
 SET GAME_FOLDER=%~p0Game
 SET PROJECT_NAME=SpaceInvaders
 SET ENGINE_NAME=Engine
@@ -7,6 +8,10 @@ SET ENGINE_NAME=Engine
 :: Clean Build Folder
 cd %BUILD_FOLDER%
 del /q *.exe
+
+:: Copy Resources
+if not exist %BUILD_FOLDER%\Resources mkdir %BUILD_FOLDER%\Resources
+xcopy %RESOURCES_FOLDER% %BUILD_FOLDER%\Resources /E /Q /D /Y
 
 :: Build Engine
 cd %ENGINE_FOLDER%
